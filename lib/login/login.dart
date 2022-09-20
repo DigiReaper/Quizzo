7b1dd3bf96b3d31f85d28334b18ddb7d09523a2d
digireaper <adithya.rayala@gmail.com>
2022-09-20 16:21:40 +0530

    basic UI

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,16 +1,76 @@
+import 'package:Quizzo/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Login extends StatelessWidget {
-const Login({ Key? key }) : super(key: key);
+  const Login({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login '),
+        title: const Text('Login'),
+      ),
+
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch ,
+          children:  [
+              const FlutterLogo( size: 150,),
+              Flexible(
+                child: LoginButton(
+                  color: Colors.blueAccent,
+                  icon: FontAwesomeIcons.userNinja,
+                  text: 'Anonymous Login',
+                  loginMethod: () => AuthService().anonLogin(),
+                )
+              ),
+              LoginButton(
+                  color: Colors.cyanAccent,
+                  icon: FontAwesomeIcons.google,
+                  text: 'Login with google',
+                  loginMethod: () => AuthService().googleLogin(),
+                ),
+            ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final Color? color;
+  final IconData? icon;
+  final String? text;
+  final Function? loginMethod;
+
+  LoginButton({
+    super.key,
+    this.color,
+    this.icon,
+    this.text,
+    this.loginMethod,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      child: ElevatedButton.icon(
+        icon: Icon(
+          icon, color: Colors.white, size: 20, 
+        ),
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(24),
+          backgroundColor: color,
+        ),
+        onPressed: () => loginMethod!(),
+        label: const Text("Login"),
       ),
     );
   }
--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,13 +1,24 @@
+import 'package:Quizzo/services/auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class Profile extends StatelessWidget {
-const Profile({ Key? key }) : super(key: key);
+  const Profile({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Container(
-      
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+      ),
+      body: ElevatedButton(
+        child: Text('Signout'),
+        onPressed: () async {
+          await AuthService().signOut();
+          Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+        },
+      ),
     );
   }
 }
